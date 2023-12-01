536f2dcaca7074f19cf9c8d187f651bc6a5cb07e
Bondhan Kimbalazani <bondhankimbalazani@improbable.io>
2023-12-01 14:43:54 +0000

    Day 1-2

--- Day1/Day1.py
+++ Day1/Day1.py
@@ -2,6 +2,17 @@ import os
 
 inputName = "input"
 sum = 0
+numberDict = {
+    'one' : 1,
+    'two' : 2, 
+    'three' : 3, 
+    'four' : 4, 
+    'five' : 5, 
+    'six' : 6, 
+    'seven' : 7, 
+    'eight' : 8, 
+    'nine' : 9
+}
 
 def GetInputTextFile():
     currentDir = os.path.dirname(os.path.realpath(__file__))
@@ -18,22 +29,55 @@ def ReadInputText():
             ProcessLine(line)
     print(sum)
 
+def CheckIfKeyInString(dict, line, array, indexArray):
+    for key in dict.keys():
+        index = line.find(key)
+        while index != -1:
+            array.append(str(dict[key]))
+            indexArray.append(index)
+            index = line.find(key, index + 1)
+    sortedNumber, sortedIndex = SortArrayPair(array, indexArray)
+    return sortedNumber, sortedIndex
+
+def SortArrayPair(numberArray, indexArray):
+    m = len(numberArray)
+    for i in range(m):
+        for j in range(0, m-i-1):
+            if (indexArray[j] > indexArray[j+1]):
+                indexArray[j], indexArray[j+1] = indexArray[j+1], indexArray[j]
+                numberArray[j], numberArray[j+1] = numberArray[j+1], numberArray[j]
+    return numberArray, indexArray
+
+def BubbleSort(array):
+    for i in range (len(array)):
+        for j in range (0, n - i -1):
+            if array[j] > array[j+1]:
+                array[j], array[j+1] = array[j+1], array[j]
+
 def ProcessLine(line):
     global sum
     strippedLine = line.strip()
     numberArray = []
-    for char in strippedLine:
-        if char.isdigit():
-            numberArray.append(char)
-    if len(numberArray) == 1:
-        newNumber = int(str(numberArray[0]) + str(numberArray[0]))
-        sum += newNumber
-        print (sum)
-    elif len(numberArray) > 1:
-        firstNumber = numberArray[0]
-        lastNumber = numberArray[-1]
-        newNumber = int(str(firstNumber) + str(lastNumber))
-        sum += newNumber
-        print (sum)
+    indexArray = []
+    result =[]
 
-ReadInputText()
+    result = [(char, index) for index, char in enumerate(strippedLine) if char.isdigit()]
+
+    for i in range(len(result)):
+        numberArray.append(result[i][0])
+        indexArray.append(result[i][1])    
+
+    sortedNumberArray, sortedIndex = CheckIfKeyInString(numberDict, strippedLine, numberArray, indexArray)
+    
+    if len(sortedNumberArray) == 1:
+        newNumber = int((sortedNumberArray[0]) + (sortedNumberArray[0]))
+        sum += newNumber
+
+    elif len(numberArray) > 1:
+        firstNumber = sortedNumberArray[0]
+        lastNumber = sortedNumberArray[-1]
+        newNumber = int((firstNumber) + (lastNumber))
+        sum += newNumber
+
+if __name__ == "__main__":
+    ReadInputText()
