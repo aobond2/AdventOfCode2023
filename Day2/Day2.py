8aa42b04157c17ea513b66bffa2c9f3b2f104759
bondhan <aobond2@gmail.com>
2023-12-03 18:15:41 +0000

    update

--- Day2/Day2.py
+++ Day2/Day2.py
@@ -6,6 +6,7 @@ cubeRuleDict = {
     'green' : 13,
     'blue' : 14
 }
+sum = 0
 
 def GetInputTextFile():
     currentDir = os.path.dirname(os.path.realpath(__file__))
@@ -21,19 +22,22 @@ def ReadInputText():
         for line in file:
             line = line.strip()
             ProcessLine(line)
+    print (sum)
 
 def ProcessLine(line):
-    passCheck = 0
-    cubeSetDict = {
-        'red' : 0,
-        'green' : 0,
-        'blue' : 0
-    }
+    global sum
+    failCheck = 0
+    print (line)
     separateLine = line.split(':')
     gameID = (separateLine[0].split('Game '))[1].strip()
     cubeSets = separateLine[1].strip()
     cubeSetsArray = (cubeSets.split(';'))
     for cubeSet in cubeSetsArray:
+        cubeSetDict = {
+                'red' : 0,
+                'green' : 0,
+                'blue' : 0
+            }
         cubeSet = cubeSet.strip()
         individualCubeArray = cubeSet.split(',')
         for individualCube in individualCubeArray:
@@ -41,15 +45,21 @@ def ProcessLine(line):
             individualCubeKey = (individualCube.split())[1]
             individualCubeValue = (individualCube.split())[0]
             cubeSetDict[individualCubeKey] = individualCubeValue
-        print (cubeSetDict)
         x = compareDictionary(cubeRuleDict, cubeSetDict)
-        print(x)
+        print (cubeSetDict)
+        failCheck += x
+    print (failCheck)
+    
+    if failCheck == 0:
+        sum += int(gameID)
+        #print (line)
 
 def compareDictionary(baseDictionary, targetDictionary):
+    # Check if any value is larger than base dictionary
     for key in baseDictionary:
-        if key in targetDictionary and int(targetDictionary[key]) > int(baseDictionary[key]):
-            return True
-        return False
+        if (int(targetDictionary[key]) > int(baseDictionary[key])) or (int(targetDictionary[key]) == int(baseDictionary[key])):
+            return 1
+        return 0
 
 if __name__ == "__main__":
     ReadInputText()
