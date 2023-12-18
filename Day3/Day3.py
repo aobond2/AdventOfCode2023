267796db2afec0a26180116a63610a5f1c766ea2
bondhan <aobond2@gmail.com>
2023-12-18 14:56:20 +0000

    Get number and its order

--- Day3/Day3.py
+++ Day3/Day3.py
@@ -2,6 +2,8 @@ import os
 import re
 
 inputName = "input"
+numberArray = []
+
 def GetInputTextFile():
     currentDir = os.path.dirname(os.path.realpath(__file__))
     fileInDir = os.listdir(currentDir)
@@ -15,12 +17,29 @@ def ReadInputText():
     with open(input, 'r') as file:
         for line in file:
             line = line.strip()
-            na = checkNonAlphaNumeric(line)
-            print (na)
+            #na = checkNonAlphaNumeric(line)
+            checkNumeric(line)
 
 def checkNonAlphaNumeric(string):
     matches = re.findall(r"[^\w\d\.]", string)
     return matches
+
+def checkNumeric(string):
+    result = []
+    numbersList = []
+    orderList = []
+    for i in range(len(string)):
+        if string[i].isdigit():
+            numbersList.append(int(string[i]))
+            orderList.append(i)
+        elif string[i] == '.':
+            if numbersList:
+                # Combine number and order list to a tuple
+                combinedTuple = (numbersList, orderList)
+                result.append(combinedTuple)
+                numbersList = []
+                orderList = []
+    print(result)
 
 if __name__ == "__main__":
     ReadInputText()
